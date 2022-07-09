705af475cf9ef959babdf842376ebfd7c8922d02
pacourbet <pierreandre.courbet@gmail.com>
2022-07-09 13:59:06 +0200

    Update load_file.py

--- google-drive-interface/load_file.py
+++ google-drive-interface/load_file.py
@@ -7,7 +7,7 @@ load_dotenv()
 gauth = GoogleAuth()           
 drive = GoogleDrive(gauth)
 
-upload_file_list = ['data_report.html', 'report_data_plot.html']
+upload_file_list = ['report.html', 'boxplot.png']
 for upload_file in upload_file_list:
 	gfile = drive.CreateFile({'parents': [{'id': os.environ.get("GDRIVE_FOLDER_ID")}]})
 	# Read file and set it as the content of this instance.
