5205d5f3e5f57dcd67b836b7a033ab7468bf8791
pacourbet <pierreandre.courbet@gmail.com>
2022-07-14 01:19:30 +0200

    Update urls.py

--- htaApp/api/urls.py
+++ htaApp/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('mesures', views.get_data),
+    path('mesures/', views.get_data),
     path('add/', views.add_mesure)
 ]
